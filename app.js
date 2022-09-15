e1c22054aa95c51aefabce847b1eeb22890010ca
AC-Galen <lottaie7359882@gmail.com>
2022-09-15 21:36:49 +0800

    feat: add index page

--- app.js
+++ app.js
@@ -3,9 +3,24 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+
 const app = express()
 const PORT = process.env.PORT
+
+const routes = require('./routes')
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
+
+module.exports = app

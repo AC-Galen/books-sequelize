98a81d21c3af28a8dc33bd7581582d780487794d
AC-Galen <lottaie7359882@gmail.com>
2022-09-16 20:37:01 +0800

    feat: auth-helper add ensureAuthenticated

--- helpers/auth-helpers.js
+++ helpers/auth-helpers.js
@@ -1,6 +1,10 @@
 const getUser = req => {
   return req.user || null // req.user ? req.user : null 是等價的，意思是若 req.user 存在就回傳 req.user，不存在的話函式就會回傳空值
 }
+const ensureAuthenticated = req => {
+  return req.isAuthenticated()
+}
 module.exports = {
-  getUser
+  getUser,
+  ensureAuthenticated
 } // 解構賦值,這邊export什麼,在其他地方的require就會是什麼

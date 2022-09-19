8346d6fdf0ff7521d8cecb232f20a6b0324acaa6
AC-Galen <lottaie7359882@gmail.com>
2022-09-19 20:05:26 +0800

    feat: category model

---
+++ migrations/20220919115510-add-category-id-to-book.js
@@ -0,0 +1,18 @@
+'use strict'
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('Books', 'category_id', {
+      type: Sequelize.INTEGER, // 對應id 是整數
+      allowNull: false, // 不允許空值
+      references: { // 寫references決定資料庫變動後,關聯是否存在
+        model: 'Categories',
+        ket: 'id'
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Books', 'category_id')
+  }
+}

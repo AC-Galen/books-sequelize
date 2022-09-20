aab176f59d1dd03798cb3be94d2677fd39eb1dd4
AC-Galen <lottaie7359882@gmail.com>
2022-09-20 22:33:15 +0800

    feat: add books index page

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -1,6 +1,20 @@
+const { Book, Category } = require('../models')
+
 const bookController = {
   getBooks: (req, res) => {
-    return res.render('books')
+    return Book.findAll({
+      include: Category, // 運用include 一並拿出關聯的Category model
+      raw: true,
+      nest: true
+    }).then(books => {
+      const data = books.map(r => ({
+        ...r, // 使用展開運算子
+        description: r.description.substring(0, 50) // 將原本的description的字串長度,修改成限定50個字
+      }))
+      return res.render('books', {
+        books: data
+      })
+    })
   }
 }
 module.exports = bookController

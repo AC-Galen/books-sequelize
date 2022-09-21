c58b79cef920ca20a366b5e82a686bc32c6f0d95
AC-Galen <lottaie7359882@gmail.com>
2022-09-21 18:12:27 +0800

    feat: add categories navbar on books index page

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -1,20 +1,31 @@
 const { Book, Category } = require('../models')
 
 const bookController = {
-  getBooks: (req, res) => {
-    return Book.findAll({
-      include: Category, // 運用include 一並拿出關聯的Category model
-      raw: true,
-      nest: true
-    }).then(books => {
-      const data = books.map(r => ({
-        ...r, // 使用展開運算子
-        description: r.description.substring(0, 50) // 將原本的description的字串長度,修改成限定50個字
-      }))
-      return res.render('books', {
-        books: data
+  getBooks: (req, res, next) => {
+    const categoryId = Number(req.query.categoryId) || '' // 從網址拿下來的參數是字串,要轉成Number在操作
+    return Promise.all([ // 兩個資料表查詢結果都回傳後再接續後面的動作,所以用Promise.all
+      Book.findAll({
+        include: Category, // 運用include 一並拿出關聯的Category model
+        where: { // 查詢條件
+          ...categoryId ? { categoryId } : {} // 檢查categoryId是否為空值
+        },
+        raw: true,
+        nest: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([books, categories]) => {
+        const data = books.map(r => ({
+          ...r, // 使用展開運算子
+          description: r.description.substring(0, 50) // 將原本的description的字串長度,修改成限定50個字
+        }))
+        return res.render('books', {
+          books: data,
+          categories,
+          categoryId
+        })
       })
-    })
+      .catch(err => next(err))
   },
   getBook: (req, res, next) => {
     return Book.findByPk(req.params.id, {
